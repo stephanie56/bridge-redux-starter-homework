00c2026c0dde31a6590984b57583084d8996c43c
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-03-04 21:48:42 -0500

    Add reducer tests

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -2,6 +2,9 @@ import { ACTION_TYPES } from '../actions';
 import { products } from './index';
 
   it('adds a product on ADD_PRODUCT', () => {
+    const intialState = {
+      productList: []
+    };
     const action = {
       type: ACTION_TYPES.addProduct,
       payload: {
@@ -10,7 +13,54 @@ import { products } from './index';
         }
       }
     };
-    expect(products([], action)).toEqual([{
-      name: 'Sofa',
-    }]);
+    expect(products(intialState, action).productList).toEqual([{name: 'Sofa'}]);
+  });
+
+  it('remove a product on REMOVE_PRODUCT', () => {
+    const intialState = {
+      productList: [{
+        id: '123',
+        name: 'table',
+      }]
+    };
+    const action = {
+      type: ACTION_TYPES.removeProduct,
+      payload: '123'
+    };
+    expect(products(intialState, action).productList).toEqual([]);
+  });
+
+  it('add a product through Form on SUBMIT_FORM', () => {
+    const intialState = {
+      productList: [],
+      product: {
+        name: 'table'
+      }
+    }
+    const action = {
+        type: ACTION_TYPES.submitForm
+      };
+    expect(products(intialState, action).productList).toEqual([{name: 'table'}]);
+  });
+
+  it('update product info on UPDATE_PRODUCT', () => {
+    const initialState = {
+      product: {
+        name: ''
+      }
+    };
+    const action = {
+      type: ACTION_TYPES.updateProduct,
+      label: 'name',
+      val: 'table'
+    };
+    expect(products(initialState, action).product.name).toEqual('table');
+  });
+
+  it('update search term on UPDATE_SEARCHTERM', () => {
+    const action = {
+      type: ACTION_TYPES.updateSearchTerm,
+      payload: 'chair'
+    };
+    expect(products({searchTerm: ''}, action).searchTerm).toEqual('chair');
   });
